338007922999d1f91f3ca3cc0565b42c77b779c3
Shane Cavanaugh <shane@shanecav.net>
2015-02-21 13:36:25 -0500

    Initial commit
    First iteration of header

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		
+		<title>ithaca$h</title>
+		
+		<link href='http://fonts.googleapis.com/css?family=Raleway:100,200,300' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/css/ithacash.css" media="screen">
+
+	</head>
+	<body>
+
+	<div id ="container">
+		<header>
+			<div id="login">
+				Login or Sign&ndash;up
+			</div>
+			<h1><img src="/img/ithacash-logo.png" alt="ithaca$h"></h1>
+
+			<div id="nav-menu">
+				<ul>
+					<li><a href="">How It Works</a></li>
+					<li><a href="">Ithaca Business</a></li>
+					<li><a href="">ithacash.org</a></li>
+				</ul>
+			</div>
+		</header>
---
+++ js/jquery.js
@@ -0,0 +1,3 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>if (!window.jQuery) { document.write('<script src="js/jquery-1.11.1.js"><\/script>'); }</script>
+<script src="/js/ithacash.js"></script>
