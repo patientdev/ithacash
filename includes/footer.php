240eac0ba5fdd46b988a45fda1dd8320c4dc7ea9
Shane Cavanaugh <shane@shanecav.net>
2015-03-18 19:10:44 -0400

    Added placeholders for further branding copy

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
-	</content>
+		</content>
+
+	</div>
 	
 	<?php include $_SERVER["DOCUMENT_ROOT"] . "/js/jquery.js"; ?>
 	<script src="/js/ithacash.js"></script>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $styles = "";
 include $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php"; ?>
 
 
-<div class="row" id="branding-pitch">
+<div class="row even" id="branding-pitch">
 	<div id="top-angle">
 		<div id="angle-1"></div>
 	</div>
@@ -18,13 +18,36 @@ include $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php"; ?>
 			Aesthetic hashtag quinoa, kogi narwhal chillwave sustainable Williamsburg pork belly. Thundercats listicle Shoreditch post-ironic hoodie twee, Banksy scenester VHS High Life umami vegan kogi retro.
 		</div>
 	</div>
-
-	<div class="branding-img">
-	</div>
 </div>
 
-<div class="row">
+<div class="row odd">
+	<div class="statement-container">
+		<div class="statement">
+			Aesthetic hashtag quinoa, kogi narwhal chillwave sustainable Williamsburg pork belly. Thundercats listicle Shoreditch post-ironic hoodie twee, Banksy scenester VHS High Life umami vegan kogi retro.
+		</div>
+		<h2>Made for Main St.</h2>
+	</div>
+	<div class="branding-image"></div>
+</div>
 
+<div class="row even">
+	<div class="statement-container">
+		<div class="statement">
+			Aesthetic hashtag quinoa, kogi narwhal chillwave sustainable Williamsburg pork belly. Thundercats listicle Shoreditch post-ironic hoodie twee, Banksy scenester VHS High Life umami vegan kogi retro.
+		</div>
+		<h2>Made for Main St.</h2>
+	</div>
+	<div class="branding-image"></div>
+</div>
+
+<div class="row odd">
+	<div class="statement-container">
+		<div class="statement">
+			Aesthetic hashtag quinoa, kogi narwhal chillwave sustainable Williamsburg pork belly. Thundercats listicle Shoreditch post-ironic hoodie twee, Banksy scenester VHS High Life umami vegan kogi retro.
+		</div>
+		<h2>Made for Main St.</h2>
+	</div>
+	<div class="branding-image"></div>
 </div>
 
 <?php include $_SERVER["DOCUMENT_ROOT"] . "/includes/footer.php"; ?>
