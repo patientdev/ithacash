591eaa8b305290e891d02ee1e6a0abdf147e9c6b
Shane Cavanaugh <shane@shanecav.net>
2015-07-15 17:06:19 -0400

    Moving signup to apps folder

--- ithacash_dev/apps/signup/urls.py
+++ ithacash_dev/apps/signup/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from ithacash_dev.views import *
+from signup.views import *
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', front),
-    url(r'^apply/', apply),
+    url(r'^$', 'apps.signup.front'),
+    url(r'^apply/', 'apps.signup.apply'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
