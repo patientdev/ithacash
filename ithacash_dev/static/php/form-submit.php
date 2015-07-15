7926a7abc1789df926a308e1f9b8da5f2e6166da
Shane Cavanaugh <shane@shanecav.net>
2015-07-15 15:25:06 -0400

    Reorganize to remove virtualenv

---
+++ ithacash_dev/static/php/form-submit.php
@@ -0,0 +1,44 @@
+<?php
+
+if ( isset($_POST) ) {
+	$which = $_POST["which"];
+
+	$to = "support@ithacash.com";
+	$email = $_POST["email"];
+	$name = $_POST["name"];
+	$from = $_POST["name"] . " <" . $email . ">";
+	$subject = "";
+
+	$body = "<!doctype html><html><head><title>$subject</title></head><body><table>";
+	foreach ( $_POST as $key => $value ) {
+		if ( $key != "which" ) {		
+			$body .= "<tr><td style=\"text-align: right;\"><strong>". ucwords($key) . ":</strong></td><td>$value</td></tr>";
+		}
+	}
+	$body .="</table></body></html>";
+
+	switch($which) {
+
+		case "email":
+			$subject = "Ithacash email sign-up";
+			break;
+
+		case "message":
+			$subject = "Ithacash message";
+			break;
+
+		case "apply":
+			$subject = "Ithacash Application";
+			break;
+	}
+
+	$header = "From: $from\r\n"; 
+	$header.= "MIME-Version: 1.0\r\n"; 
+	$header.= "Content-Type: text/html; charset=utf-8\r\n"; 
+	$header.= "X-Priority: 1\r\n";
+
+	if ( !mail($to, $subject, $body, $header) ) { echo "fail"; }
+	else { echo "Received!"; }
+}
+
+?>
