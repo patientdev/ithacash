4b966515b14708694fee1f4a1061ebfefd442e1e
Shane Cavanaugh <shane@shanecav.net>
2015-07-15 23:37:33 -0400

    Application form now completly generated as ModelForm

--- signup/views.py
+++ signup/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from signup.models import *
 from django.core.mail import send_mail
+import csv
 
 def front(request):
 	return render(request, 'front.html')
@@ -15,12 +16,21 @@ def apply(request):
 			message = ''
 
 			for key, value in request.POST.iteritems():
-				key = key.upper()
-				message+="%key: %value"
+				if value:
+					key = key.upper()
+					message+=key + ": " + value + "\n"
 
-			send_mail('Ithacash Application', message, request.POST['email'], ['shane@patientdev.com'])
+			# for row in cvs.reader
+			# 	csv.reader()
+
+			# print message
+
+		else:
+			print form.error()
+
+		# send_mail('Ithacash Application', message, request.POST['email'], ['shane@patientdev.com'])
 
 	else:
 		form = SignUpForm()
 
-	return render(request, 'apply.html', { 'form': form })
+	return render(request, 'apply.html', { 'form': form.as_p() })
