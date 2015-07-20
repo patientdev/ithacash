235eaf18865f546ffc3dd2da9ee2567cc5655b2d
Shane Cavanaugh <shane@patientdev.com>
2015-07-20 12:59:33 -0400

    Redirect applicant to '/thanks' on success

--- signup/utils.py
+++ signup/utils.py
@@ -1,24 +1,26 @@
 import os, csv
 from datetime import datetime
 
-def CSVify(dict, csvfile):
+def CyclosCSV(inputs, csvfile):
 
 	cyclos_fieldnames = ['name', 'username', 'email', 'password', 'creationdate', 'broker', 'images', 'address[identifier].name', 'address[identifier].line1', 'address[identifier].line2', 'adress[identifier].neighborhood', 'address[identifier].pobox', 'address[identifier].zip', 'address[identifier].city', 'address[identifier].region', 'address[identifier].country', 'address[identifier].private', 'mobile[identifier].name', 'mobile[identifier].number', 'mobile[identifier].private', 'landline[identifier].name', 'landline[identifier].number', 'landline[identifier].private',]
 
+	# Create dict for Cyclos CSV and populate headings
 	cyclos_input = {}
 	cyclos_input.fromkeys(cyclos_fieldnames);
 
-	cyclos_input['name'] = dict['company_name']
-	cyclos_input['email'] = dict['email']
-	cyclos_input['username'] = dict['login_name']
+	# Map form input names to required Cyclos headings
+	cyclos_input['name'] = inputs['company_name']
+	cyclos_input['email'] = inputs['email']
+	cyclos_input['username'] = inputs['login_name']
 	cyclos_input['creationdate'] = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-	cyclos_input['address[identifier].line1'] = dict['address_1']
-	cyclos_input['address[identifier].line2'] = dict['address_2']
-	cyclos_input['address[identifier].city'] = dict['city']
-	cyclos_input['address[identifier].region'] = dict['state']
-	cyclos_input['address[identifier].zip'] = dict['zip_code']
-	cyclos_input['mobile[identifier].number'] = dict['mobile_phone']
-	cyclos_input['landline[identifier].number'] = dict['landline_phone']
+	cyclos_input['address[identifier].line1'] = inputs['address_1']
+	cyclos_input['address[identifier].line2'] = inputs['address_2']
+	cyclos_input['address[identifier].city'] = inputs['city']
+	cyclos_input['address[identifier].region'] = inputs['state']
+	cyclos_input['address[identifier].zip'] = inputs['zip_code']
+	cyclos_input['mobile[identifier].number'] = inputs['mobile_phone']
+	cyclos_input['landline[identifier].number'] = inputs['landline_phone']
 
 	if not os.path.isfile(csvfile):
 		f = open(csvfile, 'a')
@@ -32,5 +34,27 @@ def CSVify(dict, csvfile):
 
 	rows = {}
 	rows.update(cyclos_input.iteritems())
+
+	writer.writerow(rows)
+
+def IthacashCSV(inputs, csvfile):
+
+	fieldnames = []
+
+	for key in inputs.keys():
+		fieldnames.append(key)
+
+	if not os.path.isfile(csvfile):
+		f = open(csvfile, 'a')
+		writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+		writer.writeheader()
+
+	else:
+		f = open(csvfile, 'a')
+		writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+	rows = {}
+	rows.update(inputs.iteritems())
 
 	writer.writerow(rows)
