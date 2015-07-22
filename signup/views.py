695ec8cb0d77715f2d1ea4a76e7b42516e3c916c
Shane Cavanaugh <shane@patientdev.com>
2015-07-22 13:34:47 -0400

    Various changes per jmyles's code review
    * Updated .gitignore to include db.sqlite3 and *.csv
    * Indention fixes per pep8 coding standard
    * Moved CSS and JS to seperate files
    * Fixed issue with long page height bc of various form heights

--- signup/views.py
+++ signup/views.py
@@ -5,40 +5,67 @@ import csv, os, random, string
 from django.http import HttpResponseRedirect, JsonResponse
 from signup.utils import CyclosCSV, IthacashCSV
 
+
 def front(request):
-	return render(request, 'front.html')
+    return render(request, 'front.html')
+
 
 def account(request):
-	form = SignUpForm()
-	context = { 'form': form }
-	return render(request, 'account.html', context)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            # Append plain text data to CSV for Cyclos import
+            # Then generate a temp pasword for them to replace in Cyclos
+            # Then kick to /thanks/
+
+            # We're not showing the tin_last4 field on the application form
+            # Let's auto-gen tin_last4 from incoming tin
+            obj = form.save(commit=False)
+            obj.tin_last4 = request.POST['tin'][-4:]
+            obj.save()
+
+            temp_password = ''.join(random.choice(string.ascii_letters) for i in range(12))
+
+            CyclosCSV(request.POST.copy(), 'signup/data/cyclos.csv')
+            IthacashCSV(request.POST.copy(), 'signup/data/ithacash.csv')
+
+            return (JsonResponse({'success': 'true'}))
+
+        else:
+            return (JsonResponse({'errors': form.errors.as_json()}))
+
+    else:
+        form = SignUpForm()
+    return render(request, 'account.html', {'form': form})
+
 
 def apply(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
 
-		if form.is_valid():
-			# Append plain text data to CSV for Cyclos import
-			# Then generate a temp pasword for them to replace in Cyclos
-			# Then kick to /thanks/
+        if form.is_valid():
+            # Append plain text data to CSV for Cyclos import
+            # Then generate a temp pasword for them to replace in Cyclos
+            # Then kick to /thanks/
 
-			# We're not showing the tin_last4 field on the application form
-			# Let's auto-gen tin_last4 from incoming tin
-			obj = form.save(commit=False)
-			obj.tin_last4 = request.POST['tin'][-4:]
-			obj.save()
+            # We're not showing the tin_last4 field on the application form
+            # Let's auto-gen tin_last4 from incoming tin
+            obj = form.save(commit=False)
+            obj.tin_last4 = request.POST['tin'][-4:]
+            obj.save()
 
-			temp_password = ''.join(str(v) for v in random.sample(range(0, 9), 5))
+            temp_password = ''.join(random.choice(string.ascii_letters) for i in range(12))
 
-			CyclosCSV(request.POST.copy(), 'signup/data/cyclos.csv')
-			IthacashCSV(request.POST.copy(), 'signup/data/ithacash.csv')
+            CyclosCSV(request.POST.copy(), 'signup/data/cyclos.csv')
+            IthacashCSV(request.POST.copy(), 'signup/data/ithacash.csv')
 
-			return (JsonResponse({ 'success': 'true' }))
+            return (JsonResponse({'success': 'true'}))
 
-		else:
-			return (JsonResponse({ 'errors': form.errors.as_json() }))
+        else:
+            return (JsonResponse({'errors': form.errors.as_json()}))
 
-	else:
-		form = SignUpForm()
+    else:
+        form = SignUpForm()
 
-	return render(request, 'apply.html', { 'form': form.as_p() })
+    return render(request, 'apply.html', {'form': form.as_p()})
