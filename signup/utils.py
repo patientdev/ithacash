2a8abb838831ff275c3e6b1a22cde9749a70342c
jMyles <justin@justinholmes.com>
2015-07-22 22:46:10 -0400

    Class-based CSV writer.  Unit test.

--- signup/utils.py
+++ signup/utils.py
@@ -1,69 +1,111 @@
-import os, csv
+import os
+import csv
 from datetime import datetime
 
-def CyclosCSV(inputs, csvfile):
+CYCLOS_FIELDNAMES = ('name', 'username', 'email', 'password', 'creationdate', 'broker', 'images',
+                         'address[identifier].name', 'address[identifier].line1', 'address[identifier].line2',
+                         'address[identifier].neighborhood', 'address[identifier].pobox', 'address[identifier].zip',
+                         'address[identifier].city', 'address[identifier].region', 'address[identifier].country',
+                         'address[identifier].private', 'mobile[identifier].name', 'mobile[identifier].number',
+                         'mobile[identifier].private', 'landline[identifier].name', 'landline[identifier].number',
+                         'landline[identifier].private')
 
-	cyclos_fieldnames = ['name', 'username', 'email', 'password', 'creationdate', 'broker', 'images', 'address[identifier].name', 'address[identifier].line1', 'address[identifier].line2', 'adress[identifier].neighborhood', 'address[identifier].pobox', 'address[identifier].zip', 'address[identifier].city', 'address[identifier].region', 'address[identifier].country', 'address[identifier].private', 'mobile[identifier].name', 'mobile[identifier].number', 'mobile[identifier].private', 'landline[identifier].name', 'landline[identifier].number', 'landline[identifier].private',]
+CYCLOS_CSV_USER_SIGNUP_MODEL_MAPPING = {  # Move this to a more sane place, perhaps to the model.
+    'name':                        'name_business',
+    'email':                       'email',  # Do we need this?
+    'username':                    'name_login',
+    'creationdate':                'created',
+    'address[identifier].line1':   'address_1',
+    'address[identifier].line2':   'address_2',
+    'address[identifier].city':    'address_city',
+    'address[identifier].region':  'address_state',
+    'address[identifier].zip':     'address_zip_code',
+    'mobile[identifier].number':   'phone_mobile',
+    'landline[identifier].number': 'phone_landline'
+}
 
-	# Create dict for Cyclos CSV and populate headings
-	cyclos_input = {}
-	cyclos_input.fromkeys(cyclos_fieldnames);
 
-	# Map form input names to required Cyclos headings
-	cyclos_input['name'] = inputs['name_business']
-	cyclos_input['email'] = inputs['email']
-	cyclos_input['username'] = inputs['name_login']
-	cyclos_input['creationdate'] = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-	cyclos_input['address[identifier].line1'] = inputs['address_1']
-	cyclos_input['address[identifier].line2'] = inputs['address_2']
-	cyclos_input['address[identifier].city'] = inputs['address_city']
-	cyclos_input['address[identifier].region'] = inputs['address_state']
-	cyclos_input['address[identifier].zip'] = inputs['address_zip_code']
-	cyclos_input['mobile[identifier].number'] = inputs['phone_mobile']
-	cyclos_input['landline[identifier].number'] = inputs['phone_landline']
+class CyclosCsvWriter(object):
+    '''
+    Writes a CSV file, mapped to cyclos taxonomy.
+    '''
 
-	if not os.path.isfile(csvfile):
-		f = open(csvfile, 'a')
-		writer = csv.DictWriter(f, fieldnames=cyclos_fieldnames)
+    def __init__(self, signup_object, csvfile_path, *args, **kwargs):
+        super(CyclosCsvWriter, self).__init__(*args, **kwargs)
 
-		writer.writeheader()
+        self.mapped = False
+        self.cyclos_field_dict = dict.fromkeys(CYCLOS_FIELDNAMES)
+        self.form_input_dict = signup_object.__dict__
+        self.csvfile_path = csvfile_path
 
-	else:
-		f = open(csvfile, 'a')
-		writer = csv.DictWriter(f, fieldnames=cyclos_fieldnames)
+    def map_dict_to_cyclos_fields(self, form_dict=None):
+        '''
+        Iterate through all field names needed for proper Cyclos CSV,
+        get corresponding values from self.form_input_dict,
+        use them to populate self.cyclos_field_dict
+        '''
+        form_dict = form_dict or self.form_input_dict
 
-	rows = {}
-	rows.update(cyclos_input.iteritems())
+        for cyclos_field_name, form_field_name in CYCLOS_CSV_USER_SIGNUP_MODEL_MAPPING.iteritems():
 
-	writer.writerow(rows)
+            try:
+                new_value = form_dict[form_field_name]
+            except KeyError:
+                raise TypeError("The form didn't have a key '%s', which is required to map the data to a Cyclos CSV." % form_field_name)
+            self.cyclos_field_dict[cyclos_field_name] = new_value
+
+        self.mapped = True
+
+        return self.cyclos_field_dict
+
+    def write_csv(self, csvfile_path=None):
+
+        if not self.mapped:
+            raise AttributeError("You must run map_dict_to_cyclos_fields before you can write the CSV file.")
+
+        csvfile_path = csvfile_path or self.csvfile_path
+
+        with open(csvfile_path, 'a') as f:
+            writer = csv.DictWriter(f, fieldnames=CYCLOS_FIELDNAMES)
+
+            if not os.path.isfile(csvfile_path):
+                writer.writeheader()
+
+            writer.writerow(self.cyclos_field_dict)
+
+
+
+
+
+
+
 
 def IthacashCSV(inputs, csvfile):
+    fieldnames = []
 
-	fieldnames = []
+    # Let's keep the last 4 digits of the TIN in plain text
+    inputs['tin_last_4'] = inputs['tin'][-4:]
 
-	# Let's keep the last 4 digits of the TIN in plain text
-	inputs['tin_last_4'] = inputs['tin'][-4:]
+    # Don't put the full TIN in at all
+    del inputs['tin']
 
-	# Don't put the full TIN in at all
-	del inputs['tin']
+    for key in inputs.keys():
+        fieldnames.append(key)
 
-	for key in inputs.keys():
-		fieldnames.append(key)
+    # At least alphabatize headings for human-readibility
+    fieldnames.sort()
 
-	# At least alphabatize headings for human-readibility
-	fieldnames.sort()
+    if not os.path.isfile(csvfile):
+        f = open(csvfile, 'a')
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-	if not os.path.isfile(csvfile):
-		f = open(csvfile, 'a')
-		writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
 
-		writer.writeheader()
+    else:
+        f = open(csvfile, 'a')
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-	else:
-		f = open(csvfile, 'a')
-		writer = csv.DictWriter(f, fieldnames=fieldnames)
+    rows = {}
+    rows.update(inputs.iteritems())
 
-	rows = {}
-	rows.update(inputs.iteritems())
-
-	writer.writerow(rows)
+    writer.writerow(rows)
