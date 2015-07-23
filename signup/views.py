09f15cf02a03f6f1694adbfbaf9611d455884d0c
Shane Cavanaugh <shane@patientdev.com>
2015-07-23 00:24:02 -0400

    Add views to get to Account Application

--- signup/views.py
+++ signup/views.py
@@ -3,7 +3,7 @@ from signup.models import *
 from django.core.mail import send_mail
 import csv, os, random, string
 from django.http import HttpResponseRedirect, JsonResponse
-
+from django.views.generic import TemplateView
 
 
 def front(request):
@@ -40,7 +40,19 @@ def account(request):
     return render(request, 'account.html', {'form': form})
 
 
-def apply(request):
+def getting_an_account(request):
+    return render(request, 'getting.html')
+
+
+def sign_up(request):
+    return render(request, 'sign-up.html')
+
+
+def sign_up_individual(request):
+    return render(request, 'sign-up-individual.html')
+
+
+def apply_individual(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
 
@@ -67,5 +79,34 @@ def apply(request):
 
     else:
         form = SignUpForm()
+    return render(request, 'apply-individual.html', {'form': form})
 
-    return render(request, 'apply.html', {'form': form.as_p()})
+
+def apply_freelancer(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            # Append plain text data to CSV for Cyclos import
+            # Then generate a temp pasword for them to replace in Cyclos
+            # Then kick to /thanks/
+
+            # We're not showing the tin_last4 field on the application form
+            # Let's auto-gen tin_last4 from incoming tin
+            obj = form.save(commit=False)
+            obj.tin_last4 = request.POST['tin'][-4:]
+            obj.save()
+
+            temp_password = ''.join(random.choice(string.ascii_letters) for i in range(12))
+
+            CyclosCSV(request.POST.copy(), 'signup/data/cyclos.csv')
+            IthacashCSV(request.POST.copy(), 'signup/data/ithacash.csv')
+
+            return (JsonResponse({'success': 'true'}))
+
+        else:
+            return (JsonResponse({'errors': form.errors.as_json()}))
+
+    else:
+        form = SignUpForm()
+    return render(request, 'apply-freelancer.html', {'form': form})
