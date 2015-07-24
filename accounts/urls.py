01035d13ba0385df68a0b90d7472bc07d956659a
jMyles <justin@justinholmes.com>
2015-07-23 20:14:54 -0400

    accounts app now installed; closes #7 closes #4

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^signup/', 'accounts.views.signup_phase_one'),
     url(r'^create_account/(?P<email_key>\w+)', 'accounts.views.create_account'),
+    url(r'^list_accounts/$', 'accounts.views.list_accounts'),
     ]
