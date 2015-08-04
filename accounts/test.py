ae22bdd4afef1ac786d9d3c688f436353a94b061
jMyles <justin@justinholmes.com>
2015-08-03 22:35:17 -0400

    Shows #29 and #30

--- accounts/test.py
+++ accounts/test.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
+from hendrix.utils.test_utils import AsyncTestMixin
 from accounts.models import Email
 
 
-class SignupTests(TestCase):
+class SignupTests(AsyncTestMixin, TestCase):
 
     def test_first_stage_give_email(self):
         address = 'dingo@dingo.com'
@@ -10,6 +11,12 @@ class SignupTests(TestCase):
 
         self.client.post('/accounts/signup/', {'address': 'dingo@dingo.com'})
         self.assertTrue(Email.objects.filter(address=address).exists())
+
+        # Now let's look at the email that's sent.
+        self.assertNumCrosstownTasks(1)
+        email_sender = self.next_task()
+
+        print email_sender
 
     def test_confirm_email(self):
         email_object = Email.objects.create(address="test@test.com")
