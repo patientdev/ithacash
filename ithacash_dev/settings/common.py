2345d3abc9414148d35c7b5e76ab62abda9cf6ac
jMyles <justin@justinholmes.com>
2015-08-04 16:21:01 -0400

    New settings layout.

--- ithacash_dev/settings/common.py
+++ ithacash_dev/settings/common.py
@@ -69,18 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ithacash_dev.wsgi.application'
 
-
-# Database
-# https:/docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https:/docs.djangoproject.com/en/1.8/topics/i18n/
 
