b06cec1e1b20f8868662a41de20e7d523aed890d
jMyles <justin@justinholmes.com>
2015-08-06 01:05:39 -0400

    Radio button for SSN/EIN.  Fixes #37.

--- payments/models.py
+++ payments/models.py
@@ -6,6 +6,6 @@ class Payment(models.Model):
 
 class SignUpPayment(Payment):
     created = models.DateTimeField(auto_now_add=True)
-    amount = models.DecimalField(max_digits=8)
+    amount = models.DecimalField(decimal_places=2, max_digits=8)
     txn_id = models.CharField(max_length=255)
 
