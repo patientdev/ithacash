7788a082d535fa771d801f60c3d5dbc3cc3756d2
Shane Cavanaugh <shane@patientdev.com>
2015-08-06 14:06:50 -0400

    Have form's parent page handle validation, then push POST values to form's action url. Closes #47.

--- ithacash_dev/static/js/jquery.js
+++ ithacash_dev/static/js/jquery.js
@@ -1,2 +1,3 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script>if (!window.jQuery) { document.write('<script src="/js/jquery-1.11.3.min.js"><\/script>'); }</script>
+<script>if (!window.jQuery) { document.write('<script src="/static/js/jquery-1.11.3.min.js"><\/script>'); }
+</script>
