3b705df2324484d82f8a95a0424164b598359a18
Shane Cavanaugh <shane@patientdev.com>
2015-08-21 23:35:59 -0400

    Form to send message is working and has error handling

--- pages/views.py
+++ pages/views.py
@@ -25,63 +25,52 @@ def front(request):
             if form.is_valid():
                 payload = {
                     'email_address': request.POST.get('subscriber_email_address'),
-                    'status': 'subscribed',
-                    'merge_fields': {
-                        'FNAME': request.POST.get('subscriber_first_name'), 
-                        'LNAME': request.POST.get('subscriber_last_name')
-                    }
+                    'status': 'subscribed'
                 }
 
                 try:
-                    r = requests.post('https://us9.api.mailchimp.com/3.0/lists/933eb82aa4/members/', auth=HTTPBasicAuth('', settings.MAILCHIMP_API_KEY), data=json.dumps(payload))
+                    r = requests.post('https://us9.api.mailchimp.com/3.0/lists/ec92ed377f/members/', auth=HTTPBasicAuth('', settings.MAILCHIMP_API_KEY), json=payload)
 
                     if r.status_code == 200:
-                        return JsonResponse({'success': True})
+                        return JsonResponse({'success': True}, status=200)
+
+                    elif r.status_code == 400:
+                        return JsonResponse({'errors': 'already exists'}, status=400)
 
                     else:
-                        return JsonResponse({'errors': r}, status=500)
+                        return JsonResponse({'errors': r.json()}, status=500)
 
                 except requests.exceptions.RequestException as e:
                     return JsonResponse({'errors': str(e)}, status=500)
 
             else:
-                return JsonResponse(form.errors, status=400);
+                return JsonResponse({'errors': form.errors}, status=400)
 
         elif which_form == 'message_form':
 
-            form = message_form(request.POST)
+            form = send_message_form(request.POST)
 
-            which = request.POST['which']
-            post_data = request.POST.copy()
-            del post_data['which']
+            if form.is_valid():
 
-            subject = 'Message from ithacash.com'
+                mandrill_client = mandrill.Mandrill(settings.MANDRILL_API_KEY)
 
-            t = loader.get_template('emails/front.txt')
-            c = Context({
-                'post_data': post_data
-            })
-            message = t.render(c)
+                try:
+                    result = mandrill_client.messages.send(
+                        {
+                            'to': [{'email': 'support@ithacash.com'}],
+                            'text': request.POST.get('message'),
+                            'from_name': 'Ithacash.com',
+                            'from_email': request.POST.get('message_from'),
+                            'subject': 'Message from ithacash.com',
+                        })
 
-            mandrill_client = mandrill.Mandrill(settings.MANDRILL_API_KEY)
+                    return (JsonResponse({'success': result}))
 
-            try:
-                mandrill_client.messages.send(
-                    {
-                        'to': [{'email': 'support@ithacash.com'}],
-                        'html': message,
-                        'from_name': 'Ithacash.com',
-                        'from_email': request.POST['email'],
-                        'subject': subject,
-                    })
-
-                return (JsonResponse({'success': True}))
-
-            except Exception, e:
-                return (JsonResponse({'errors': str(e)}, status=500))
+                except Exception, e:
+                    return (JsonResponse({'errors': str(e)}, status=500))
 
             else:
-                return (JsonResponse({'errors': 'else'}, status=500))
+                return (JsonResponse({'errors': form.errors}, status=400))
 
     else:
         return render(request, 'front.html', {'newsletter_form': newsletter_form, 'message_form': message_form})
