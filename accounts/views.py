9185ce34cfa1acdeb6122100f68e66bf5930e21b
Shane Cavanaugh <shane@patientdev.com>
2015-08-25 12:46:00 -0400

    Move user creation and email sending to await_confirmation

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from django import forms
 from accounts.models import Email, IthacashUser, IthacashAccount
 from ithacash_dev.sayings import EMAIL_ALREADY_IN_SYSTEM
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 
 class EmailForm(forms.ModelForm):
@@ -69,21 +70,13 @@ def getting_an_account(request):
     return render(request, 'getting-an-account.html')
 
 
+@csrf_exempt
 def signup_phase_one(request):
     form = EmailForm(request.POST or None)
 
     if request.method == 'POST':
 
         if form.is_valid():
-            email_object, created = Email.objects.get_or_create(address=request.POST['address'])
-
-            if not created:
-                if IthacashAccount.objects.filter(owner=email_object.owner).exists():
-                    form.add_error('address', EMAIL_ALREADY_IN_SYSTEM)
-                    return (JsonResponse(form.errors, status=400, reason="BAD REQUEST: Invalid form values"))
-
-
-            request.session['most_recent_confirmation_key'] = email_object.most_recent_confirmation_key
 
             return (JsonResponse({'success': True}, status=202, reason="OK: Form values accepted"))
 
@@ -94,14 +87,26 @@ def signup_phase_one(request):
         return render(request, 'signup-phase-one.html', {'form': form })
 
 
+@csrf_exempt
 def await_confirmation(request):
-    email_object = Email.objects.get(most_recent_confirmation_key=request.session['most_recent_confirmation_key'])
 
-    @crosstown_traffic()
-    def send_email_later():
-        email_object.send_confirmation_message()
+    if request.method == 'POST':
 
-    return render(request, 'await-confirmation.html')
+        email_object, created = Email.objects.get_or_create(address=request.POST['address'])
+
+        if not created:
+            if IthacashAccount.objects.filter(owner=email_object.owner).exists():
+                form.add_error('address', EMAIL_ALREADY_IN_SYSTEM)
+                return (JsonResponse(form.errors, status=400, reason="BAD REQUEST: Invalid form values"))
+
+        @crosstown_traffic()
+        def send_email_later():
+            email_object.send_confirmation_message()
+
+        return render(request, 'await-confirmation.html')
+
+    else:
+        return HttpResponseRedirect('/accounts/signup/')
 
 
 def purchase_ithaca_dollars(request):
