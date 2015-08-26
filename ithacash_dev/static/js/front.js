9ddc8d0be83752bdd9e7000befd52937b6e3266f
Shane Cavanaugh <shane@patientdev.com>
2015-08-26 12:53:58 -0400

    #savepoint

--- ithacash_dev/static/js/front.js
+++ ithacash_dev/static/js/front.js
@@ -1,17 +1,17 @@
 $(function() {
 	
-	$(window).scroll(function() {
+	// $(window).scroll(function() {
 
-		map_offset_top = $('#invigorating-our-economy').offset().top;
-		map_height = $('#invigorating-our-economy').height();
-		map_offset_bottom = map_offset_top + map_height
-		intro_offset = $('#intro').offset().top;
-		window_scroll = $(window).scrollTop();
+	// 	map_offset_top = $('#invigorating-our-economy').offset().top;
+	// 	map_height = $('#invigorating-our-economy').height();
+	// 	map_offset_bottom = map_offset_top + map_height
+	// 	intro_offset = $('#intro').offset().top;
+	// 	window_scroll = $(window).scrollTop();
 
-		if ( window_scroll < map_offset_bottom && window_scroll > intro_offset) {
-			background_offset = (window_scroll / 5) - 200;
-			$('#invigorating-our-economy').css('background-position', '0px ' + background_offset + 'px');
-		}
+	// 	if ( window_scroll < map_offset_bottom && window_scroll > intro_offset) {
+	// 		background_offset = (window_scroll / 5) - 200;
+	// 		$('#invigorating-our-economy').css('background-position', '0px ' + background_offset + 'px');
+	// 	}
 
 	})
 })
