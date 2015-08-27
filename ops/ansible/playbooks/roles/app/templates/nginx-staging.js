7d494b94c1313de30b872b07b882a302c9d20dc3
Shane Cavanaugh <shane@patientdev.com>
2015-08-27 16:26:44 -0400

    Set nginx server to proxy :8000

--- ops/ansible/playbooks/roles/app/templates/nginx-staging.js
+++ ops/ansible/playbooks/roles/app/templates/nginx-staging.js
@@ -3,7 +3,7 @@ server {
        server_name localhost;
 
        location / {
-               proxy_pass   http://localhost:1337; # Apache
+               proxy_pass   http://localhost:8000; # Apache
                proxy_set_header Host       $host;
        }
 }
