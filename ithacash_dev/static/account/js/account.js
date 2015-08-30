c423346852833fd32cd04fb5743291987a7fd12b
Shane Cavanaugh <shane@patientdev.com>
2015-08-30 16:37:48 -0400

    Move await_confirmation logic back

--- ithacash_dev/static/account/js/account.js
+++ ithacash_dev/static/account/js/account.js
@@ -1,48 +1,48 @@
 $(function() {
-	if ( $('#id_account_type').val() != '' ) {
-		$('#account-type-selection').css('display', 'block');
+    if ( $('#id_account_type').val() != '' ) {
+        $('#account-type-selection').css('display', 'block');
 
         if ( $('#id_account_type').val()  == 'Individual' ) {
             $('#id_entity_name').hide();
             $('#txt2pay-phone').css('display', 'none');
         }
-	}
+    }
 
     $('#id_account_type').change(function() {
-    	account_type_selection = $(this).val();
+        account_type_selection = $(this).val();
 
-    	$('.info-card').css('display', 'none');
+        $('.info-card').css('display', 'none');
 
         if ( account_type_selection  == 'Individual') {
             $('#id_entity_name').val('n/a').hide();
             $('#id_full_name').attr('placeholder', 'Your Name');
             $('#txt2pay-phone').css('display', 'none');
             $('#individual-info').css('display', 'block');
-        } 
+        }
 
         else {
 
             $('#txt2pay-phone').css('display', 'block');
 
             if ( account_type_selection == 'Standard Business' || account_type_selection == 'Premier Business' || account_type_selection == 'Freelancer') {
-           		$('#id_entity_name').attr('placeholder', 'Business Name');
+                   $('#id_entity_name').attr('placeholder', 'Business Name');
 
-           		if ( account_type_selection == 'Standard Business') {
-		            $('#standard-info').css('display', 'block');
-           		}
+                   if ( account_type_selection == 'Standard Business') {
+                    $('#standard-info').css('display', 'block');
+                   }
 
-           		else if ( account_type_selection == 'Premier Business') {
-		            $('#premier-info').css('display', 'block');
-           		}
+                   else if ( account_type_selection == 'Premier Business') {
+                    $('#premier-info').css('display', 'block');
+                   }
 
-           		else if ( account_type_selection == 'Freelancer') {
-		            $('#freelancer-info').css('display', 'block');
-		        }
-            } 
+                   else if ( account_type_selection == 'Freelancer') {
+                    $('#freelancer-info').css('display', 'block');
+                }
+            }
 
             else {
                 $('#id_entity_name').attr('placeholder', 'Organization Name');
-	            $('#nonprofit-info').css('display', 'block');
+                $('#nonprofit-info').css('display', 'block');
             }
 
             $('#id_full_name').attr('placeholder', 'Contact Name');
@@ -55,100 +55,100 @@ $(function() {
 
     $('form#account').submit(function( event ) {
 
-    	event.preventDefault();
-    	event.stopImmediatePropagation();
+        event.preventDefault();
+        event.stopImmediatePropagation();
 
-    	form = $(this);
+        form = $(this);
 
-    	data = form.serialize();
-		action_url = $(this).attr('action');
-		validation_url = window.location.pathname;
+        data = form.serialize();
+        action_url = $(this).attr('action');
+        validation_url = window.location.pathname;
 
-		// Have AJAX handle the validation
-		$.ajax({
-			url: validation_url, 
-			method: 'POST',
-			data: data
-		})
-		.fail(function( response ){
+        // Have AJAX handle the validation
+        $.ajax({
+            url: validation_url,
+            method: 'POST',
+            data: data
+        })
+        .fail(function( response ){
 
-			if ( response.status == 400 ) {
+            if ( response.status == 400 ) {
 
-	            errors = $.parseJSON(response.responseText);
+                errors = $.parseJSON(response.responseText);
 
-	            // Which fields have errors
-	            error_indices = []
-	            $.each(errors, function(index) {
-	                error_indices.push(index);
-	            })
+                // Which fields have errors
+                error_indices = []
+                $.each(errors, function(index) {
+                    error_indices.push(index);
+                })
 
-	            inputs = $(':input', form);
+                inputs = $(':input', form);
 
-	            //
-	            // Highlight form inputs that have errors
-	            //
-	            $.each(inputs, function() {
-	                input_name = $(this).attr('name');
+                //
+                // Highlight form inputs that have errors
+                //
+                $.each(inputs, function() {
+                    input_name = $(this).attr('name');
 
-	                // If the name of the input equals the error field index
-	                if ( $.inArray(input_name, error_indices) != -1 ) {
-	                    error_message = errors[input_name][0];
+                    // If the name of the input equals the error field index
+                    if ( $.inArray(input_name, error_indices) != -1 ) {
+                        error_message = errors[input_name][0];
 
-	                    // Update error message if input is still invalid
-	                    if ( $(this).next('.error-message').length > 0 ) {
-	                        $(this).next('.error-message').text(error_message);
-	                    }
+                        // Update error message if input is still invalid
+                        if ( $(this).next('.error-message').length > 0 ) {
+                            $(this).next('.error-message').text(error_message);
+                        }
 
-	                    else {
-	                        $(this).addClass('error').after('<span class="error-message">' + error_message + '</span>');
-	                    }
-	                }
+                        else {
+                            $(this).addClass('error').after('<span class="error-message">' + error_message + '</span>');
+                        }
+                    }
 
-	                // De-highlight iputs that are now valid
-	                else {
-	                    $(this).removeClass('error');
-	                    $(this).next('.error-message').remove();
-	                }
-	            })
+                    // De-highlight iputs that are now valid
+                    else {
+                        $(this).removeClass('error');
+                        $(this).next('.error-message').remove();
+                    }
+                })
 
                     $('html, body').animate({
-                    	scrollTop: $('.error-message').first().offset().top - 170
+                        scrollTop: $('.error-message').first().offset().top - 170
                     })
 
 
-	            return false;
-	        }
+                return false;
+            }
 
-	        else if ( response.status == 500 ) {
-	        	$('form#account').replaceWith('<p>An error occured. Please refresh the page and try again.</p>');
-	        }
+            else if ( response.status == 500 ) {
+                $('form#account').replaceWith('<p>An error occured. Please refresh the page and try again.</p>');
+            }
 
 
-	    })
-		.success(function(){
-		     $('form#account').unbind('submit').submit(); 
-		})
-	})
+        })
+        .success(function(){
+             $('form#account').unbind('submit').submit();
+        })
+    })
 
-	$('form#account input').focus(function() {
-		$(this).parent().siblings('.help-text').find('.text').css('opacity', '1');
-		$(this).siblings('.error-message').remove();
-	})
-	$('form#account input').blur(function() {
-		$(this).parent().siblings('.help-text').find('.text').css('opacity', '0');
-	})
+    $('form#account input').focus(function() {
+        $(this).parent().siblings('.help-text').find('.text').css('opacity', '1');
+        $(this).siblings('.error-message').remove();
+    })
+    $('form#account input').blur(function() {
+        $(this).parent().siblings('.help-text').find('.text').css('opacity', '0');
+    })
 
-	$('form#account label').hover(function() {
-		$(this).parent().siblings('.help-text').find('.text').css('opacity', '1');
-	}, function() {
-		$(this).parent().siblings('.help-text').find('.text').css('opacity', '0');
-	})
+    $('form#account label').hover(function() {
+        $(this).parent().siblings('.help-text').find('.text').css('opacity', '1');
+    }, function() {
+        $(this).parent().siblings('.help-text').find('.text').css('opacity', '0');
+    })
 
-	$('.help').hover(function() {
-		$(this).siblings('.text').css('opacity', '1');
-	}, function() {
-		$(this).siblings('.text').css('opacity', '0');
-	})
+    $('.help').hover(function() {
+        $(this).siblings('.text').css('opacity', '1');
+    }, function() {
+        $(this).siblings('.text').css('opacity', '0');
+    })
 })
 
 function mapInputsToReview(div) {
