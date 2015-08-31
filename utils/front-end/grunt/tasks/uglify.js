59ccb6bc535418afc5b69e55c647933bb8bf83f3
Shane Cavanaugh <shane@patientdev.com>
2015-08-31 00:59:34 -0400

    Fix nav styling bugs

--- utils/front-end/grunt/tasks/uglify.js
+++ utils/front-end/grunt/tasks/uglify.js
@@ -1,10 +1,10 @@
 module.exports = {
     options: {
-        banner: '/*! <%= pkg.name %>.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= package.name %>.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
     },
     global: {
         files: {
-            '<%= globalConfig.src  %>/js/patient.js': ['js/patient.dev.js']
+            '<%= django.static  %>/js/ithacash.js': ['js/*.js']
         }
     }
 };
