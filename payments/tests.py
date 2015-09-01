3109a32d32554f5d98a510afb5bfcf26766de0b2
Shane Cavanaugh <shane@patientdev.com>
2015-08-31 20:18:51 -0400

    Test for CSV output and email

--- payments/tests.py
+++ payments/tests.py
@@ -16,6 +16,7 @@ SAMPLE_PAYPAL_IPN_PAYLOAD = {
     u'shipping_discount': u'0.00',
     u'receiver_email': u'button-guy@ithacash.org', u'payment_status': u'Completed', u'payment_gross': u'1.00', u'tax': u'0.00', u'residence_country': u'US', u'cmd': u'notify-validate', u'address_state': u'CA', u'payer_status': u'verified', u'txn_type': u'web_accept', u'address_street': u'1 Main St', u'handling_amount': u'0.00', u'payment_date': u'14:27:04 Aug 05, 2015 PDT', u'first_name': u'New', u'btn_id': u'3192831', u'item_name': u'(Test) Standard Business', u'address_country': u'United States', u'charset': u'windows-1252', u'custom': u'account_1', u'notify_version': u'3.8', u'address_name': u'New Buyer', u'test_ipn': u'1', u'item_number': u'', u'receiver_id': u'XVXXCB8749N22', u'transaction_subject': u'account_1', u'business': u'button-guy@ithacash.org', u'payerid': u'2VRDXFJNECEUJ', u'discount': u'0.00', u'verify_sign': u'A-X.bgkHiBJDUeODT9kVdmzwhS1aAbhjO5OJZEUPE1ntThQFudFDZSiD', u'address_zip': u'95131', u'payment_fee': u'0.33', u'address_country_code': u'US', u'address_city': u'San Jose', u'address_status': u'confirmed', u'insurance_amount': u'0.00', u'mc_fee': u'0.33', u'mc_currency': u'USD', u'shipping': u'0.00', u'payer_email': u'new-buyer@ithacash.org', u'payment_type': u'instant', u'mc_gross': u'1.00', u'ipn_track_id': u'8f1adeb7339d', u'quantity': u'1'}
 
+
 class PaypalTests(TestCase):
 
     def test_ipn_endpoint(self):
