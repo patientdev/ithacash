ad673cc383346464e528a05e81f5fa60c1ce9821
Shane Cavanaugh <shane@ithacash.com>
2015-09-22 15:49:45 -0400

    Add class to bleach.ALLOWED_ATTRIBUTES for links

--- pages/views.py
+++ pages/views.py
@@ -146,8 +146,9 @@ def page_creator(request):
                 # Let's whitelist tags for POSTed content
                 flatpage = flatpage_form.save(commit=False)
                 bleach.ALLOWED_TAGS.extend(['p', 'mark', 'h3', 'h4'])
-                bleach.ALLOWED_ATTRIBUTES.extend(['class'])
-                flatpage.content = bleach.clean(flatpage.content, tags=bleach.ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True)
+                bleach.ALLOWED_ATTRIBUTES['a'].extend(['class'])
+                print bleach.ALLOWED_ATTRIBUTES
+                flatpage.content = bleach.clean(flatpage.content, tags=bleach.ALLOWED_TAGS, attributes=bleach.ALLOWED_ATTRIBUTES, strip=True)
                 flatpage.save()
                 flatpage_form.save_m2m()
 
