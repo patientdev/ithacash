9dfe0af3300f98e1a2c4886955dff16105812b28
Shane Cavanaugh <shane@ithacash.com>
2015-09-22 15:35:37 -0400

    Add 'class' to bleach's ALLOWED_ATTIBUTES

--- pages/views.py
+++ pages/views.py
@@ -146,7 +146,8 @@ def page_creator(request):
                 # Let's whitelist tags for POSTed content
                 flatpage = flatpage_form.save(commit=False)
                 bleach.ALLOWED_TAGS.extend(['p', 'mark', 'h3', 'h4'])
-                flatpage.content = bleach.clean(flatpage.content, tags=bleach.ALLOWED_TAGS, strip=True)
+                bleach.ALLOWED_ATTRIBUTES.extend(['class'])
+                flatpage.content = bleach.clean(flatpage.content, tags=bleach.ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True)
                 flatpage.save()
                 flatpage_form.save_m2m()
 
