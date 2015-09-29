6a374e00ea8760b156fb139f969cc4d420e53254
Shane Cavanaugh <shane@ithacash.com>
2015-09-29 12:53:11 -0400

    Forward /join to accounts signup form

--- ithacash_dev/urls.py
+++ ithacash_dev/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'pages.views.front'),
     url(r'^apply/$', RedirectView.as_view(url='/accounts/signup/')),
+    url(r'^join/$', RedirectView.as_view(url='/accounts/signup/')),
     url(r'^accounts/', include('accounts.urls')),
 
     url(r'^paypal_ipn_endpoint/', 'payments.views.paypal_ipn_endpoint', name="paypal_ipn_endpoint"),
