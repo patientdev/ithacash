936cef62147b5974dcd4679e3cbaf953e091573e
Shane Cavanaugh <shane@ithacash.com>
2015-10-08 20:29:18 -0400

    PNG and JPEG compression for grunt

--- utils/front-end/grunt/tasks/imagemin.js
+++ utils/front-end/grunt/tasks/imagemin.js
@@ -1,15 +1,26 @@
 var mozjpeg = require('imagemin-mozjpeg');
+var pngquant = require('imagemin-pngquant');
 module.exports = {
-    dynamic: {
+    png: {
         options: {
-            optimizationLevel: 7,
+            use: [pngquant({quality: '65-80', speed: 4})]
+        },
+        files: [{
+            expand: true,                       // Enable dynamic expansion
+            cwd: "img/",                        // Src matches are relative to this path
+            src: ["**/*.png"],                  // Actual patterns to match
+            dest: "<%= django.static %>/img/"   // Destination path prefix
+        }]
+    },
+    jpg: {
+        options: {
             use: [mozjpeg({quality:80})]
         },
         files: [{
-            expand: true,                  // Enable dynamic expansion
-            cwd: "img/",                   // Src matches are relative to this path
-            src: ["**/*.{png,jpg,gif}"],   // Actual patterns to match
-            dest: "<%= django.static %>/img/"                  // Destination path prefix
+            expand: true,                       // Enable dynamic expansion
+            cwd: "img/",                        // Src matches are relative to this path
+            src: ["**/*.jpg"],                  // Actual patterns to match
+            dest: "<%= django.static %>/img/"   // Destination path prefix
         }]
     }
 };
