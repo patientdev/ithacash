ad9de35b4e6da8b7328c4b3b1bbdbd172183769d
Shane Cavanaugh <shane@patientdev.com>
2015-11-15 13:16:30 -0500

    #savepoint

---
+++ staff/admin.py
@@ -0,0 +1,4 @@
+# from django.contrib import admin
+# from staff.models import IthacashStaff, StaffManager
+#
+# admin.site.register(IthacashStaff, StaffManager)
--- staff/views.py
+++ staff/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from .forms import StaffLogin, StaffSignup
 from django.http.response import JsonResponse
+from django.contrib.auth import authenticate, login
 
 
 def login(request):
-    form = StaffLogin()
-    return render(request, 'login.html', {'form': form})
+    form = StaffLogin(request.POST or None)
+
+    if request.method == 'POST':
+        email = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return JsonResponse({'success': True}, status=202, reason="OK: Form values accepted")
+
+            else:
+                return JsonResponse({'fail': True}, status=400)
+        else:
+            return JsonResponse({'fail': True}, status=404, reason="BAD REQUEST: User does not exist")
+
+    else:
+        return render(request, 'login.html', {'form': form})
 
 
 def signup(request):
@@ -15,8 +33,6 @@ def signup(request):
         if form.is_valid():
 
             user = form.save()
-
-            print user
 
             return JsonResponse({'success': True}, status=202, reason="OK: Form values accepted")
 
