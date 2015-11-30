64c25a57b7046cc86dfd7d65bf01d299582a7b1e
Shane Cavanaugh <shane@patientdev.com>
2015-11-30 15:53:25 -0500

    Rotate dot transition

--- utils/front-end/js/front.js
+++ utils/front-end/js/front.js
@@ -1,20 +1,5 @@
 $(function() {
 
-    // $(window).scroll(function() {
-
-    // 	map_offset_top = $('#invigorating-our-economy').offset().top;
-    // 	map_height = $('#invigorating-our-economy').height();
-    // 	map_offset_bottom = map_offset_top + map_height
-    // 	intro_offset = $('#intro').offset().top;
-    // 	window_scroll = $(window).scrollTop();
-
-    // 	if ( window_scroll < map_offset_bottom && window_scroll > intro_offset) {
-    // 		background_offset = (window_scroll / 5) - 200;
-    // 		$('#invigorating-our-economy').css('background-position', '0px ' + background_offset + 'px');
-    // 	}
-
-    // })
-
     $('#panels').slick({
           infinite: true,
           autoplay: true,
