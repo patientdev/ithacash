5111681ea66c981e44f0c4253d9b13c52f7a9926
Shane Cavanaugh <shane@patientdev.com>
2015-12-17 21:08:19 -0500

    Hotfix to handle direct PayPal transactions
    We were receiveing errors that PayPal was hitting our payments.views.paypal_ipn_endpoint but not POSTing a 'custom' key. Turns out, every payment made to Ithacash's PayPal account is directed to payments.views.paypal_ipn_endpoint upon completion. Through account signup, a 'custom' key containing the newly created account ID# is POSTed with the PayPal transaction then sent back to us at the end of the transaction.
    
    We now check that direct transactions are valid and treat them as such. A new test has been created for such an event.

--- payments/views.py
+++ payments/views.py
@@ -16,11 +16,12 @@ def paypal_ipn_endpoint(request):
         return HttpResponseBadRequest("This request was not validated by paypal.")
 
     if request.POST['payment_status'] == "Completed":
-        account_id = request.POST['custom'].split('_')[1]
-        account = IthacashAccount.objects.get(id=account_id)
+        if 'custom' in request.POST:
+            account_id = request.POST['custom'].split('_')[1]
+            account = IthacashAccount.objects.get(id=account_id)
 
-        payment = SignUpPayment.objects.create(account=account, amount=request.POST['payment_gross'])
+            payment = SignUpPayment.objects.create(account=account, amount=request.POST['payment_gross'])
 
-        account.send_awaiting_verification_message()
+            account.send_awaiting_verification_message()
 
     return HttpResponse("PROCESSED.")
