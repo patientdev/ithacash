f8dd7ddfc38bcda937634e9c024888f06cdbe6f1
Shane Cavanaugh <shane@patientdev.com>
2015-12-17 21:08:19 -0500

    Hotfix to handle direct PayPal transactions
    We were receiveing errors that PayPal was hitting our payments.views.paypal_ipn_endpoint but not POSTing a 'custom' key. Turns out, every payment made to Ithacash's PayPal account is directed to payments.views.paypal_ipn_endpoint upon completion. Through account signup, a 'custom' key containing the newly created account ID# is POSTed with the PayPal transaction then sent back to us at the end of the transaction.
    
    We now check that direct transactions are valid and treat them as such. A new test has been created for such an event.

--- payments/tests.py
+++ payments/tests.py
@@ -14,14 +14,14 @@ SAMPLE_PAYPAL_IPN_PAYLOAD = {
     u'txn_id': u'2FL55751505704848',
     u'shipping_method': u'Default',
     u'shipping_discount': u'0.00',
-    u'receiver_email': u'button-guy@ithacash.org', u'payment_status': u'Completed', u'payment_gross': u'1.00', u'tax': u'0.00', u'residence_country': u'US', u'cmd': u'notify-validate', u'address_state': u'CA', u'payer_status': u'verified', u'txn_type': u'web_accept', u'address_street': u'1 Main St', u'handling_amount': u'0.00', u'payment_date': u'14:27:04 Aug 05, 2015 PDT', u'first_name': u'New', u'btn_id': u'3192831', u'item_name': u'(Test) Standard Business', u'address_country': u'United States', u'charset': u'windows-1252', u'custom': u'account_1', u'notify_version': u'3.8', u'address_name': u'New Buyer', u'test_ipn': u'1', u'item_number': u'', u'receiver_id': u'XVXXCB8749N22', u'transaction_subject': u'account_1', u'business': u'button-guy@ithacash.org', u'payerid': u'2VRDXFJNECEUJ', u'discount': u'0.00', u'verify_sign': u'A-X.bgkHiBJDUeODT9kVdmzwhS1aAbhjO5OJZEUPE1ntThQFudFDZSiD', u'address_zip': u'95131', u'payment_fee': u'0.33', u'address_country_code': u'US', u'address_city': u'San Jose', u'address_status': u'confirmed', u'insurance_amount': u'0.00', u'mc_fee': u'0.33', u'mc_currency': u'USD', u'shipping': u'0.00', u'payer_email': u'new-buyer@ithacash.org', u'payment_type': u'instant', u'mc_gross': u'1.00', u'ipn_track_id': u'8f1adeb7339d', u'quantity': u'1'}
+    u'receiver_email': u'button-guy@ithacash.org', u'payment_status': u'Completed', u'payment_gross': u'1.00', u'tax': u'0.00', u'residence_country': u'US', u'cmd': u'notify-validate', u'address_state': u'CA', u'payer_status': u'verified', u'txn_type': u'web_accept', u'address_street': u'1 Main St', u'handling_amount': u'0.00', u'payment_date': u'14:27:04 Aug 05, 2015 PDT', u'custom': u'account_1', u'first_name': u'New', u'btn_id': u'3192831', u'item_name': u'(Test) Standard Business', u'address_country': u'United States', u'charset': u'windows-1252', u'notify_version': u'3.8', u'address_name': u'New Buyer', u'test_ipn': u'1', u'item_number': u'', u'receiver_id': u'XVXXCB8749N22', u'transaction_subject': u'account_1', u'business': u'button-guy@ithacash.org', u'payerid': u'2VRDXFJNECEUJ', u'discount': u'0.00', u'verify_sign': u'A-X.bgkHiBJDUeODT9kVdmzwhS1aAbhjO5OJZEUPE1ntThQFudFDZSiD', u'address_zip': u'95131', u'payment_fee': u'0.33', u'address_country_code': u'US', u'address_city': u'San Jose', u'address_status': u'confirmed', u'insurance_amount': u'0.00', u'mc_fee': u'0.33', u'mc_currency': u'USD', u'shipping': u'0.00', u'payer_email': u'new-buyer@ithacash.org', u'payment_type': u'instant', u'mc_gross': u'1.00', u'ipn_track_id': u'8f1adeb7339d', u'quantity': u'1'}
 
 
 class PaypalTests(TestCase):
 
-    def test_ipn_endpoint(self):
-        print 'test_ipn_endpoint'
-        
+    def test_ipn_endpoint_new_account(self):
+        print 'test_ipn_endpoint_new_account'
+
         r = RequestFactory()
         r.method = "POST"
         r.POST = SAMPLE_PAYPAL_IPN_PAYLOAD
@@ -46,3 +46,20 @@ class PaypalTests(TestCase):
 
                 self.assertEqual(mock_paypal_ipn.call_count, 1)
                 self.assertEqual(mock_email_sender.call_count, 1)
+
+    def test_ipn_endpoint_direct_transation(self):
+        print 'test_ipn_endpoint_direct_transation'
+
+        '''For PayPal transactions initiated directly through PayPal rather than at the end of our signup form'''
+
+        r = RequestFactory()
+        r.method = "POST"
+        r.POST = SAMPLE_PAYPAL_IPN_PAYLOAD
+        r.META = {}
+
+        # Direct transactions won't recieve and therefore won't send a 'custom' POST key, so remove it from our sample payload
+        r.POST.pop('custom')
+
+        with patch.object(PaypalValidator, 'validate_paypal_ipn', return_value=True) as mock_paypal_ipn:
+            response = paypal_ipn_endpoint(r)
+            self.assertEqual('PROCESSED.', response.content)
