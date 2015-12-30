42c3e95927324d05001191fcd6c2f2076cca3d46
Shane Cavanaugh <shane@patientdev.co>
2015-12-30 01:58:58 -0500

    Merge migrations

---
+++ accounts/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0017_auto_20151230_0645'),
+        ('accounts', '0008_merge'),
+    ]
+
+    operations = [
+    ]
