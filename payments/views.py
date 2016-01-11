2a159b563bebfb8a3b488de49a2eae03831cc6d3
Shane Cavanaugh <shane@patientdev.co>
2016-01-11 14:19:37 -0500

    Test for register_account API call

--- payments/views.py
+++ payments/views.py
@@ -4,23 +4,22 @@ from accounts.models import IthacashAccount
 from payments.models import SignUpPayment
 from payments.utils import PaypalValidator
 
+from django.shortcuts import redirect
+
 
 @csrf_exempt
 def paypal_ipn_endpoint(request):
     is_legit = PaypalValidator().validate_paypal_ipn(request.POST.copy())
 
-    if is_legit:
-        pass
-
-    else:
-        return HttpResponseBadRequest("This request was not validated by paypal.")
-
-    if request.POST['payment_status'] == "Completed":
+    if is_legit and request.POST['payment_status'] == "Completed":
         account_id = request.POST['custom'].split('_')[1]
         account = IthacashAccount.objects.get(id=account_id)
 
         payment = SignUpPayment.objects.create(account=account, amount=request.POST['payment_gross'])
 
-        account.send_awaiting_verification_message()
+        return redirect('accounts.api.register_account', id=account.id)
+
+    else:
+        return HttpResponseBadRequest("This request was not validated by paypal.")
 
     return HttpResponse("PROCESSED.")
