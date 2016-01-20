dc20e50de1615d379e6a3766c0e71dbd6f170806
Shane Cavanaugh <shane@patientdev.co>
2016-01-19 23:54:06 -0500

    Dashboard init

--- staff/urls.py
+++ staff/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^login/', 'staff.views.login_staff'),
     url(r'^signup/', 'staff.views.signup'),
-    url(r'^(?P<staff_id>\w+)', 'staff.views.dashboard'),
+    url(r'^(?P<staff_id>\d*)', 'staff.views.dashboard'),
 ]
