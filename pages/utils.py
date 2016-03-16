e2815f5f3dd76ff622aefa8814c7a711809308d4
Shane Cavanaugh <shane@patientdev.com>
2016-03-16 13:50:04 -0400

    Add source code editing to Page Creator
    Clean up pages app utils.py to move bleaching out of the view

--- pages/utils.py
+++ pages/utils.py
@@ -1,37 +1,13 @@
 import bleach
 
+def PageCreatorBleaching(flatpage_content):
 
-def PageCreatorPostProcessing(content):
+    bleach.ALLOWED_TAGS.extend(['p', 'mark', 'h3', 'h4', 'br', 'img', 'input', 'button'])
+    bleach.ALLOWED_ATTRIBUTES['a'].extend(['class', 'target'])
+    bleach.ALLOWED_ATTRIBUTES['img'] = ['src', 'height', 'width']
+    bleach.ALLOWED_ATTRIBUTES['input'] = ['type', 'id', 'name', 'value', 'placeholder']
+    bleach.ALLOWED_ATTRIBUTES['form'] = ['id', 'action']
 
-    processed_content = ''
+    bleached_content = bleach.clean(flatpage.content, tags=bleach.ALLOWED_TAGS, attributes=bleach.ALLOWED_ATTRIBUTES, strip=True)
 
-    # <p>
-    paragraphs = ''
-    new_content = ''
-
-    tags = ['<h3>', '</h3>', '<ul>', '</ul>', '<li>', '</li>', '<blockquote>', '</blockquote>']
-
-    for double_break in content.split('\r\n\r\n'):
-
-        print "%s - %s" % (double_break, all(tag not in double_break for tag in tags))
-
-        if any(tag not in double_break for tag in tags):
-            double_break = '<p>%s</p>\n\n' % double_break
-
-        paragraphs += double_break
-
-    for single_break in paragraphs.split('\r\n'):
-
-        if any(tag not in single_break for tag in tags):
-            single_break = '%s<br>\n' % single_break
-
-        new_content += single_break
-
-    processed_content += new_content.rstrip().rstrip('<br>')
-
-    return processed_content
-
-
-def PageCreatorPreProcessing(content):
-
-    return bleach.clean(content, tags=[None], strip=True)
+    return bleached_content
