96d0e9f69750a68e878e1630cfd5b56de1cb396e
Shane Cavanaugh <shane@patientdev.com>
2016-03-18 15:45:09 -0400

    AJAX for standard-form with specific responses for MailChimp

--- pages/api.py
+++ pages/api.py
@@ -2,14 +2,13 @@ import requests
 from django.conf import settings
 from .forms import add_to_mailing_list_form
 from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
 
 
 @csrf_exempt
 def add_to_mailing_list(request):
 
     form = add_to_mailing_list_form(request.POST or None)
-
-    print form
 
     if request.method == 'POST' and form.is_valid():
 
@@ -26,18 +25,19 @@ def add_to_mailing_list(request):
         try:
             r = requests.post(mailchimp_endpoint, auth=requests.auth.HTTPBasicAuth('', settings.MAILCHIMP_API_KEY), json=payload)
 
+            print r.status_code
+            
             if r.status_code == 200:
-                return JsonResponse({'success': True}, status=200)
+                return JsonResponse({'success': 'true'}, status=200)
 
             elif r.status_code == 400:
-                return JsonResponse({'errors': 'already exists'}, status=400)
+                return JsonResponse({'email_address': ['Email is already signed up']}, status=400)
 
             else:
-                print r.json()
-                return JsonResponse({'errors': r.json()}, status=500)
+                return JsonResponse({'email_address': ['There was an error on our part. We have been automatically notified.']}, status=500)
 
         except requests.exceptions.RequestException as e:
             return JsonResponse({'errors': str(e)}, status=500)
 
     else:
-        return JsonResponse({'errors': form.errors}, status=400)
+        return JsonResponse(form.errors, status=400)
--- utils/front-end/js/ithacash.js
+++ utils/front-end/js/ithacash.js
@@ -31,68 +31,6 @@ $(function() {
         })
     })
 
-    $('#send-message').submit(function( event ) {
-        event.preventDefault();
-
-        data = $(this).serialize();
-        validation_url = window.location.pathname;
-
-        $.ajax({
-            url: validation_url,
-            method: 'POST',
-            data: data,
-            statusCode: {
-                200: function() {
-                    $('#send-message').replaceWith('<p>Thanks for the message!</p>');
-                },
-
-                400: function() {
-
-                    $('#send-message').append('<p class="error">Please fill out both inputs.</p>');
-                },
-
-                500: function() {
-                    $('#send-message button').replaceWith('<p class="error">An error occured. Please refresh the page and try again.</p>');
-                }
-            }
-        })
-    });
-
-    $('#join-email-list').submit(function( event ) {
-        event.preventDefault();
-
-        data = $(this).serialize();
-        validation_url = window.location.pathname;
-
-        $.ajax({
-            url: validation_url,
-            method: 'POST',
-            data: data,
-            statusCode: {
-                200: function() {
-                    $('#join-email-list').replaceWith('<p class="error">Thanks for signing up!</p>');
-                },
-
-                400: function( response ) {
-
-                    errors = $.parseJSON(response.responseText);
-
-                    if ( errors.errors == 'already exists' ) {
-                        $('#join-email-list').replaceWith('<p class="error">It looks like you&rsquo;re already signed&ndash;up! Check your spam folder if you&rsquo;re not receiving our newsletter.</p>');
-                    }
-
-                    else {
-                        $('#join-email-list').append('<p class="error">Please submit your email address.');
-                    }
-                },
-
-                500: function() {
-                    $('#join-email-list').replaceWith('<p class="error">An error occured. Please refresh the page and try again.</p>');
-                }
-            }
-        })
-    });
-
     $('form.standard-form').submit(function( event ) {
 
         event.preventDefault();
@@ -102,11 +40,10 @@ $(function() {
 
         data = form.serialize();
         action_url = $(this).attr('action');
-        validation_url = window.location.pathname;
 
         // Have AJAX handle the validation
         $.ajax({
-            url: validation_url,
+            url: action_url,
             method: 'POST',
             data: data
         })
@@ -123,7 +60,6 @@ $(function() {
                 error_indices = []
                 $.each(errors, function(index) {
                     error_indices.push(index);
-                    console.log(index);
                 })
 
                 inputs = $(':input', form);
@@ -144,7 +80,7 @@ $(function() {
                         }
 
                         else {
-                            $(this).addClass('error').after('<span class="error-message">' + error_message + '</span>');
+                            $(this).addClass('error').after('<br><span class="error-message">' + error_message + '</span>');
                         }
                     }
 
@@ -164,13 +100,13 @@ $(function() {
             }
 
             else if ( response.status == 500 ) {
-                $('form#account').replaceWith('<p>An error occured. Please refresh the page and try again.</p>');
+                $('form.standard-form').replaceWith('<p>An error occured. Please refresh the page and try again.</p>');
             }
 
 
         })
         .success(function(){
-             $('form#account').unbind('submit').submit();
+             $('form.standard-form').html('<p>Thanks!</p>');
         })
     })
 
