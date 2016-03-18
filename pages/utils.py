fe00c7e88b65ea4382767b7300f26f384b573ea6
Shane Cavanaugh <shane@patientdev.com>
2016-03-18 15:45:09 -0400

    AJAX for standard-form with specific responses for MailChimp

--- pages/utils.py
+++ pages/utils.py
@@ -6,7 +6,8 @@ def PageCreatorBleaching(flatpage_content):
     bleach.ALLOWED_ATTRIBUTES['a'].extend(['class', 'target'])
     bleach.ALLOWED_ATTRIBUTES['img'] = ['src', 'height', 'width']
     bleach.ALLOWED_ATTRIBUTES['input'] = ['type', 'id', 'name', 'value', 'placeholder']
-    bleach.ALLOWED_ATTRIBUTES['form'] = ['id', 'action', 'method']
+    bleach.ALLOWED_ATTRIBUTES['form'] = ['id', 'action', 'method', 'class']
+    bleach.ALLOWED_ATTRIBUTES['button'] = ['type']
 
     bleached_content = bleach.clean(flatpage_content, tags=bleach.ALLOWED_TAGS, attributes=bleach.ALLOWED_ATTRIBUTES, strip=True)
 
